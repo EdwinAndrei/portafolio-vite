b4348d295db2658a3534c1928a635c01505b2419
EdwinAndrei <159513149+EdwinAndrei@users.noreply.github.com>
2025-11-07 18:04:35 -0600

    Vite

--- vite.config.js
+++ vite.config.js
@@ -4,32 +4,27 @@ import * as glob from "glob";
 
 function obtenerEntradas() {
     return Object.fromEntries(
-        glob.sync(
-            './*.html',
-            {
-                ignore: [
-                    './dist/**',
-                    './node_modules/**'
-                ]
-            }
-        ).map((file) => {
-            return [
-                file.replace('.html', ''),
-                resolve(__dirname, file)
-            ];
-        })
+        glob.sync('./*.html', {
+            ignore: ['./dist/**', './node_modules/**']
+        }).map((file) => [
+            file.slice(0, file.length - path.extname(file).length),
+            resolve(__dirname, file)
+        ])
     );
 }
 
 export default defineConfig({
     appType: 'mpa',
-    base: process.env.DEPLOY_BASE_URL || '/portafolio-vite/',
+
+    base: "/portafolio-vite/",
 
     build: {
+        outDir: "dist",
+        assetsDir: "assets",
         minify: true,
+
         rollupOptions: {
-            input: obtenerEntradas()
-        },
-        outDir: 'dist'
+            input: obtenerEntradas(),
+        }
     },
 });
