4462515121bd825545e602ce71a8906007445f52
EdwinAndrei <159513149+EdwinAndrei@users.noreply.github.com>
2025-11-07 21:06:48 -0600

    prueba

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,34 @@
-import { defineConfig } from 'vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import path, { resolve } from "node:path";
+import * as glob from "glob";
+
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+import HtmlCssPurgePlugin from "vite-plugin-purgecss";
+
+function obtenerEntradas() {
+  return Object.fromEntries(
+    glob
+      .sync("./**/*.html", {
+        ignore: ["./dist/**", "./node_modules/**"],
+      })
+      .map((file) => [
+        file.slice(0, file.length - path.extname(file).length),
+        resolve(__dirname, file),
+      ])
+  );
+}
 
 export default defineConfig({
-  base: process.env.DEPLOY_BASE_URL,
+  appType: "mpa",
+  base: process.env.DEPLOY_BASE_URL ,
   build: {
-    
+    minify: true,
     rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, 'index.html'),
-        biografia: path.resolve(__dirname, 'biografia.html'),
-        contactos: path.resolve(__dirname, 'contactos.html'),
-      },
-    },
-    outDir: 'dist',
-    assetsDir: 'assets',
-    cssCodeSplit: true,
-    minify: 'esbuild',
-  },
-
-  css: {
-    preprocessorOptions: {
-      less: {},
+      input: obtenerEntradas(),
     },
   },
-})
+  plugins: [
+    HtmlCssPurgePlugin(),
+    ViteMinifyPlugin(),
+  ],
+});
