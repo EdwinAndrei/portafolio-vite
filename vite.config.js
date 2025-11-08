65716c97d154803035ca1c61b3055874ec5150ac
EdwinAndrei <159513149+EdwinAndrei@users.noreply.github.com>
2025-11-07 20:54:34 -0600

    prueba

--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,25 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
- base: process.env.DEPLOY_BASE_URL,
+  base: process.env.DEPLOY_BASE_URL,
   build: {
-
+    
     rollupOptions: {
       input: {
         index: path.resolve(__dirname, 'index.html'),
         biografia: path.resolve(__dirname, 'biografia.html'),
-        contactos: path.resolve(__dirname, 'contactos.html')
-      }
+        contactos: path.resolve(__dirname, 'contactos.html'),
+      },
     },
     outDir: 'dist',
     assetsDir: 'assets',
     cssCodeSplit: true,
-    minify: 'esbuild'
+    minify: 'esbuild',
   },
-  
+
   css: {
     preprocessorOptions: {
-      less: {}
-    }
-  }
+      less: {},
+    },
+  },
 })
