93109df10b14e0ac2eac30dd6d7160e50c2f7299
EdwinAndrei <159513149+EdwinAndrei@users.noreply.github.com>
2025-11-07 19:28:59 -0600

    final

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ function obtenerEntradas() {
 export default defineConfig({
     appType: 'mpa',
 
-    base: "/portafolio-vite/",
+    base: process.env.DEPLOY_BASE_URL,
 
     build: {
         outDir: "dist",
